54919a5b9fb8bb71b8e5599e083e4680862744b6
Wissam Salah Almsalati <w.almsalati@uot.edu.ly>
2026-07-14 22:49:04 +0200

    initialize commit

---
+++ src/test/java/com/example/logicore/LogiCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logicore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogiCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
